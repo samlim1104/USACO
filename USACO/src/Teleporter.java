6f37b5c53a4864c7e899e91a2669794e64f2be42
samlim1104 <samlim1104@gmail.com>
2022-06-14 17:49:24 -0700

    Create Teleporter

---
+++ USACO/src/Teleporter.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Teleporter {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader in = new BufferedReader(new FileReader("teleport.in"));
+		PrintWriter out = new PrintWriter("teleport.out");
+		
+		String[] input = in.readLine().split(" ");
+		
+		int a = Integer.parseInt(input[0]);
+		int b = Integer.parseInt(input[1]);
+		int x = Integer.parseInt(input[2]);
+		int y = Integer.parseInt(input[3]);
+		
+		int result = Math.abs(a-b);
+		result = Math.min(result, Math.abs(b-x) + Math.abs(a-y));
+		result = Math.min(result, Math.abs(b-y) + Math.abs(a-x));
+		
+		out.print(result);
+		
+		in.close();
+		out.close();
+	}
+}
