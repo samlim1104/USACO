b96638e90e0627bda57a8c63e05ebe6af75af1c7
samlim1104 <samlim1104@gmail.com>
2022-06-21 18:59:26 -0700

    Update USACOShellGame.java

--- Bronze/src/USACOShellGame.java
+++ Bronze/src/USACOShellGame.java
@@ -10,22 +10,24 @@ public class USACOShellGame {
 		int[] shellPosition = new int[3];
 		int[] guess = new int[3];
 		
+		shellPosition[0] = 0;
+		shellPosition[1] = 1;
+		shellPosition[2] = 2;
+		
 		for(int i = 0; i<swaps; i++) {
 			int a = in.nextInt()-1;
 			int b = in.nextInt()-1;
 			int g = in.nextInt()-1;
 			
-			shellPosition[0] = 1;
-			shellPosition[1] = 2;
-			shellPosition[2] = 3;
-			
 			int t = shellPosition[a];
 			shellPosition[a] = shellPosition[b];
 			shellPosition[b] = t;
 			
-			guess[g]++;
+			guess[shellPosition[g]]++;
 		}
 		
 		out.println(Math.max(guess[0], Math.max(guess[1], guess[2])));
+		in.close();
+		out.close();
 	}
 }
