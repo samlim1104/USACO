914cfd849712de2b33bc297a8421d8b4400b240e
samlim1104 <samlim1104@gmail.com>
2022-06-28 17:07:58 -0700

    Create USACOBlockedBillboard2.java

---
+++ Bronze/src/USACOBlockedBillboard2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class USACOBlockedBillboard2 {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new FileReader("billboard.in"));
+		PrintWriter out = new PrintWriter("billboard.out");
+		
+		int b1BLx = in.nextInt();
+		int b1BLy = in.nextInt();
+		int b1TRx = in.nextInt();
+		int b1TRy = in.nextInt();
+		
+		int b2BLx = in.nextInt();
+		int b2BLy = in.nextInt();
+		int b2TRx = in.nextInt();
+		int b2TRy = in.nextInt();
+		
+		int tBLx = in.nextInt();
+		int tBLy = in.nextInt();
+		int tTRx = in.nextInt();
+		int tTRy = in.nextInt();
+		
+		int numCorner1 = 0;
+		int numCorner2 = 0;
+		
+		
+	}
+}
