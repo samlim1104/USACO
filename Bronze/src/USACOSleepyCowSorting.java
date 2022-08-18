1cdf07d07aa38a1b36533389af933f7a488836a0
samlim1104 <samlim1104@gmail.com>
2022-08-17 19:59:41 -0700

    USACOSleepyCiowSorting

--- Bronze/src/USACOSleepyCowSorting.java
+++ Bronze/src/USACOSleepyCowSorting.java
@@ -1,4 +1,32 @@
-
+import java.util.*;
+import java.io.*;
 public class USACOSleepyCowSorting {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new FileReader("sleepy.in"));
+		PrintWriter out = new PrintWriter("sleepy.out");
+		
+		int n = in.nextInt();
+		
+		int[] array = new int[n];
+		
+		for(int i = 0; i<n; i++) {
+			array[i] = in.nextInt();
+		}
 
+		boolean result = false;
+		
+		for(int a = array.length-1; a>=1; a--) {
+			if(array[a-1] > array[a]) {
+				result = true;
+				out.println(a);
+				break;
+			}
+		}
+		
+		if(!result) {
+			out.println(0);
+		}
+		out.close();
+in.close();
+	}
 }
