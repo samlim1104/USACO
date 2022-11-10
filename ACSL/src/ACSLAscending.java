efaad340ede1093533c7ae84a12c897bcb8497f4
samlim1104 <samlim1104@gmail.com>
2022-11-09 20:09:48 -0800

    ACSLAscending

--- ACSL/src/ACSLAscending.java
+++ ACSL/src/ACSLAscending.java
@@ -1,4 +1,33 @@
+import java.util.*;
 
 public class ACSLAscending {
-
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		for(int i = 0; i<5; i++) {
+			String input = in.next();
+			
+			int index = Integer.parseInt(input.substring(0, 1));
+			
+			String result = input.substring(1, 1+index);
+			
+			int sub = result.length() + 1;
+			
+			String add = "";
+			int before = Integer.parseInt(result);
+			
+			for(int r = input.length(); r>sub; r--) {
+				add += input.substring(r-1, r);
+				if(Integer.parseInt(add) > before) {
+					result += " " + add;
+					before = Integer.parseInt(add);
+					add = "";
+				}
+			}
+			
+			System.out.println(result);
+		}
+		
+		
+	}
 }
---
+++ ACSL/src/ACSLEnclosure.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class ACSLEnclosure {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		for(int i = 0; i<5; i++) {
+			String input = in.next();
+			
+			String result = "";
+		
+			if(input.indexOf("[") > 0 && input.indexOf("]") > 0) {
+				if(input.indexOf("(") > 0) {
+					for(int r = input.indexOf("(")+2; r<input.indexOf("]"); r++) {
+						if(r < input.indexOf("]")-1 && isNumeric(input.substring(r, r+1)) && isNumeric(input.substring(r+1, r+2))) {
+							result += r+3 + ", ";
+						}
+						else if(isNumeric(input.substring(r, r+1))) {
+							result += r+2 + ", ";
+						}
+					}
+					System.out.println(result.substring(0, result.length()-2));
+				}
+				else if(input.indexOf(")") > 0) {
+					for(int r = input.indexOf("["); r<input.indexOf(")")-1; r++) {
+						if(r < input.indexOf(")")-2 && isNumeric(input.substring(r, r+1)) && isNumeric(input.substring(r+1, r+2))) {
+							result += r+1 + ", ";
+						}
+						else if(isNumeric(input.substring(r, r+1))) {
+							result += r+1 + ", ";
+						}
+					}
+					System.out.println(result.substring(0, result.length()-2));
+				}
+			}
+		}
+	}
+	
+	public static boolean isNumeric(String str) {
+		if(str == null) {
+			return false;
+		}
+		else {
+			try {
+				int num = Integer.parseInt(str);
+			}catch(NumberFormatException e) {
+				return false;
+			}
+			return true;
+		}
+	}
+}
